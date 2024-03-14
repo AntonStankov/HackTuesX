cd5bf17466630ef4160235a6a2f99873d1a377c0
AntonStankov <antonstankov007@gmail.com>
2024-03-14 16:46:09 -0400

    create api gateway and deployed to the cloud via Dockerfile

---
+++ ApiGateway/eureka_v3/src/test/java/com/example/eureka_v3/EurekaV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
