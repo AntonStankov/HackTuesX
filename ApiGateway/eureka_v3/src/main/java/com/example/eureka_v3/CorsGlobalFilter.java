51317ce22cd25c798728f689fb7bd7b8be32e5a3
AntonStankov <antonstankov007@gmail.com>
2024-03-14 19:18:47 -0400

    added cors to apigateway

---
+++ ApiGateway/eureka_v3/src/main/java/com/example/eureka_v3/CorsGlobalFilter.java
@@ -0,0 +1,29 @@
+package com.example.eureka_v3;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CorsGlobalFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            if (exchange.getRequest().getHeaders().containsKey("Origin")) {
+                exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
+                exchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+                exchange.getResponse().getHeaders().add("Access-Control-Allow-Headers", "*");
+                exchange.getResponse().getHeaders().add("Access-Control-Max-Age", "3600");
+                if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+                    exchange.getResponse().setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(exchange);
+        };
+    }
+}
